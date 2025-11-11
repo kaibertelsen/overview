7afb7074278eb6cd306277c1e66ddddeb89da3c7
Kai Bertelsen <kai@exposoft.no>
2025-11-11 10:48:42 +0100

    ny

--- startup.js
+++ startup.js
@@ -38,12 +38,14 @@ function responsgetClient(data,id){
    localStorage.setItem("clientobject", JSON.stringify(data.fields));  
    updateClientdata();  
 
+   /*
    //oppdater lable
    if(data.fields?.Name){
    document.getElementById("clientnamelable").textContent = data.fields.Name
    }else{
     document.getElementById("clientnamelable").textContent = "-"
    }
+    */
 }
 
 
@@ -114,3 +116,114 @@ function responsprojectlistCompany(data,id){
       }
 }
 
+
+function gettime(date){
+    var d = new Date(date);
+   
+   if (typeof(date) == "undefined"){
+   d = new Date();
+   }
+    
+   
+     let year = d.getFullYear();
+   
+     let mnd = findmontname(d.getMonth());
+     let day = d.getDate();
+   
+   
+     var hour = d.getHours();
+     if(hour<10){
+     hour = "0"+hour;
+     }
+   
+     var min = d.getMinutes();
+     if(min<10){
+     min = "0"+min;
+     }
+   
+     var sec = d.getSeconds();
+     if(sec<10){
+     sec = "0"+sec;
+     }
+     return day+"."+mnd+" "+year;
+   }
+   
+   function updatetime(){
+   
+     document.getElementById("updatetime").innerHTML = gettime(ontrackMonitoring);
+     
+   }
+   
+   
+   function updateClientdata(){
+          
+        var clientobject = JSON.parse(localStorage.getItem("clientobject"));      
+        const list = document.getElementById("sumview");
+
+        //oppdater lable
+        if(clientobject.Name){
+        document.getElementById("clientnamelable").textContent = clientobject.Name
+        }
+     
+       //customer
+       const customer = list.getElementsByClassName("customer")[0];
+       var customervalue = round(Number(clientobject.sumcustomervalue), 1);
+       customervalue = valutalook(customervalue);
+       customer.innerHTML = customervalue+"K";  
+          
+       //supplier
+       const supplier = list.getElementsByClassName("supplier")[0];
+       var suppliervalue = round(Number(clientobject.sumsuppliervalue), 1);
+       suppliervalue = valutalook(suppliervalue);
+       supplier.innerHTML = suppliervalue+"K";   
+       
+       //difference   
+       const difference = list.getElementsByClassName("difference")[0];
+       var differencevalue = round(Number(clientobject.diffsuppliercustomer), 1);
+       differencevalue = valutalook(differencevalue);
+       difference.innerHTML = differencevalue+"K";    
+       
+       //profit
+       const profit = list.getElementsByClassName("profit")[0];
+       var profitvalue = round(Number(clientobject.profittoday), 1);
+       profitvalue = valutalook(profitvalue);
+       profit.innerHTML = profitvalue+"K"; 
+       
+     
+        //statusbar
+         const selectfield = list.getElementsByClassName("selectfield")[0];
+         var ontrackvalue = clientOntrackToDate(clientobject)*100;
+          if(ontrackvalue<0){
+             ontrackvalue = 0;
+           }
+           
+         selectfield.style.width = ontrackvalue+"%";  
+         
+         
+       //year budget
+       const lowbudgetyear = list.getElementsByClassName("lowbudget")[0];
+       var lowbudgetyearvalue = round(Number(clientobject.yearbudgetlow), 1);
+       lowbudgetyearvalue = valutalook(lowbudgetyearvalue);
+       lowbudgetyear.innerHTML = lowbudgetyearvalue+"K"; 
+       
+       //lowbudgettoday
+       const lowbudgettoday = list.getElementsByClassName("lowbudgettoday")[0];
+       var lowbudgettodayvalue = round(BudgetToDate(clientobject,clientobject.yearbudgetlow), 1);
+       lowbudgettodayvalue = valutalook(lowbudgettodayvalue);
+       lowbudgettoday.innerHTML = lowbudgettodayvalue+"K"; 
+       
+         
+       const highbudgetyear = list.getElementsByClassName("highbudget")[0];
+       var highbudgetyearvalue = round(Number(clientobject.yearbudgethight), 1);
+       highbudgetyearvalue = valutalook(highbudgetyearvalue);
+       highbudgetyear.innerHTML = highbudgetyearvalue+"K";   
+       
+          
+       const hightbudgettoday = list.getElementsByClassName("hightbudgettoday")[0];
+       var hightbudgettodayvalue = round(BudgetToDate(clientobject,clientobject.yearbudgethight), 1);
+       hightbudgettodayvalue = valutalook(hightbudgettodayvalue);
+       hightbudgettoday.innerHTML = hightbudgettodayvalue+"K"; 
+         
+   
+             
+   }
